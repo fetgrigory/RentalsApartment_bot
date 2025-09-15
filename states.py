36d464aef87cad40b189965aa7edb4e1856c8053
fetgrigory <157891679+fetgrigory@users.noreply.github.com>
2025-09-15 21:14:08 +0300

    feat: migrate to modular architecture with routers
    refactor handlers to use Router instead of Dispatcher
    
    move FSM states to separate states.py module
    
    improve code structure for better scalability and maintainability

---
+++ states.py
@@ -0,0 +1,43 @@
+'''
+This module make
+
+Athor: Fetkulin Grigory, Fetkulin.G.R@yandex.ru
+Starting 15/09/2025
+Ending //
+'''
+from aiogram.fsm.state import StatesGroup, State
+
+
+# States group
+class AddApartmentState(StatesGroup):
+    PHOTO1 = State()
+    PHOTO2 = State()
+    PHOTO3 = State()
+    DESCRIPTION = State()
+    ADDRESS = State()
+    PRICE = State()
+    CATEGORY = State()
+
+
+class EditApartmentState(StatesGroup):
+    PHOTO1 = State()
+    PHOTO2 = State()
+    PHOTO3 = State()
+    DESCRIPTION = State()
+    ADDRESS = State()
+    PRICE = State()
+    CATEGORY = State()
+
+
+class BookingState(StatesGroup):
+    FIRST_NAME = State()
+    LAST_NAME = State()
+    PHONE = State()
+
+
+class ReviewState(StatesGroup):
+    TEXT = State()
+
+
+class QuestionState(StatesGroup):
+    WAITING_QUESTION = State()
