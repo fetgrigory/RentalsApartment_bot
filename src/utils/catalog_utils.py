76f11932821e44931cf57bd6ef381468e0716979
fetgrigory <97grfetkulin@mail.ru>
2026-04-17 20:39:02 +0300

    refactor(catalog): decouple catalog_utils from data layer

--- src/utils/catalog_utils.py
+++ src/utils/catalog_utils.py
@@ -1,25 +1,10 @@
-'''
-This bot make
-
-Author: Fetkulin Grigory, Fetkulin.G.R@yandex.ru
-Starting 19/09/2025
-Ending //
-'''
-# Installing the necessary libraries
 from aiogram import types
 from src.keyboards.admin_keyboard import catalog_navigation_edit_keyboard
 from src.keyboards.user_keyboard import catalog_navigation_keyboard
-from src.db.crud import get_catalog_data
 
 
-# Global dictionary for storing user data
-USER_DATA = {}
-
-
-# Apartment data display function
-async def show_apartment_data(message: types.Message, apartments=None, index=0, edit_mode=False):
-    if apartments is None:
-        apartments = get_catalog_data()
+# Render apartment data for catalog
+async def show_apartment_data(message: types.Message, apartments, index=0, edit_mode=False):
     if not apartments:
         await message.answer("Каталог пуст!")
         return
@@ -27,10 +12,12 @@ async def show_apartment_data(message: types.Message, apartments=None, index=0, 
     record = apartments[index]
 
     photos_info = [
-        types.InputMediaPhoto(media=getattr(record, f'photo{i}'), caption=f"Фото квартиры")
+        types.InputMediaPhoto(
+            media=getattr(record, f'photo{i}'),
+            caption=f"Фото квартиры"
+        )
         for i in range(1, 4)
     ]
-    # Creating an apartment card for display in the catalog
     message_text = (
         f"🏠 Описание:\n{record.description}\n\n"
         f"📍 Адрес:\n{record.address}\n\n"
@@ -41,11 +28,11 @@ async def show_apartment_data(message: types.Message, apartments=None, index=0, 
         f"💰 Цена (в сутки): {record.price} ₽"
     )
 
-    # Choose the keyboard depending on the mode
-    keyboard = catalog_navigation_edit_keyboard(index, len(apartments)) if edit_mode else catalog_navigation_keyboard(index, len(apartments))
+    keyboard = (
+        catalog_navigation_edit_keyboard(index, len(apartments))
+        if edit_mode
+        else catalog_navigation_keyboard(index, len(apartments))
+    )
 
     await message.bot.send_media_group(message.chat.id, media=photos_info)
     await message.answer(message_text, reply_markup=keyboard)
-    # Save data to global variable
-    USER_DATA['apartments'] = apartments
-    USER_DATA['apartment_index'] = index
