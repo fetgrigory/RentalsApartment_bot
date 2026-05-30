ce576296cf1dac7b0b6dd800f4a841e719dc1121
fetgrigory <97grfetkulin@mail.ru>
2026-05-30 18:16:10 +0300

    feat(apps): add core and room django applications

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.core',
+    'apps.room',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
