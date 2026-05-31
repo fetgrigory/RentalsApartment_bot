098621f7f6c48696d1767094dc3b4164cb3c4a17
fetgrigory <97grfetkulin@mail.ru>
2026-05-31 21:31:36 +0300

    refactor(rooms): rename app room → rooms and rebuild migrations

--- config/settings.py
+++ config/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'apps.core',
-    'apps.room',
+    'apps.rooms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
