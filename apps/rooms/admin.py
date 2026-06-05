79ccae867f0bfef4bf3a57f93a145807c071d0c2
fetgrigory <97grfetkulin@mail.ru>
2026-06-05 17:37:05 +0300

    feat(admin): add room_number to list display

--- apps/rooms/admin.py
+++ apps/rooms/admin.py
@@ -4,6 +4,6 @@ from .models import Room
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
-    list_display = ("id", "category", "price")
+    list_display = ("id", "room_number", "category", "price")
     list_filter = ("category",)
     search_fields = ("description",)
