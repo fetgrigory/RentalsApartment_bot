567a0d9d93306778e1efc16a4b16ba1ad356904f
fetgrigory <97grfetkulin@mail.ru>
2026-06-10 01:15:19 +0300

    feat(apps): add users django application

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'apps.core',
     'apps.rooms',
+    'apps.users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
